7187dc0b5ef8fa27f1bb1030065fdcded4f4f4a0
OmarVillalobos <omar_vr@live.com.mx>
2017-08-28 21:24:24 -0500

    advance ggplot, some findings

--- Mcklein_Caja.R
+++ Mcklein_Caja.R
@@ -18,18 +18,32 @@ datos <- read.csv("CAJA-JUL-AGO.csv", header = TRUE)
 datos$Fecha <- as.character(datos$Fecha)
 datos$Fecha <- as.POSIXct(strptime(datos$Fecha,"%d/%m/%y %I:%M %p"))
 datos$horas <- as.factor(hour(datos$Fecha))
+datos <- datos[datos$Ventas.netas > 0,]
+datos <- datos[datos$Ventas.netas < 500,]
 
+
+mea <- mean(datos$Ventas.netas)
+devup <- mea + sd(datos$Ventas.netas)
+devdow <- mea - sd(datos$Ventas.netas)
+  
 ggplot(datos, aes(x=Ventas.netas,fill=horas)) +
   geom_histogram(aes(y=..density..), binwidth = 20)
 
+ggplot(datos, aes(horas,Ventas.netas), fill= Total.recaudado) + 
+  geom_boxplot() + 
+  scale_fill_gradient(low = "red", high = "yellow") +
+  geom_hline(yintercept = mea, size= 1, colour="#FF3721",
+             linetype = "dashed",show.legend = TRUE) + 
+  geom_hline(yintercept = devup, size= 1, colour="#053FC9",
+             linetype = "dashed",show.legend = TRUE) + 
+  geom_hline(yintercept = devdow, size= 1, colour="#053FC9",
+             linetype = "dashed",show.legend = TRUE) + 
+  scale_y_continuous(limits = c(0,500)) + 
+  annotate("text", x = 450, , label = "Some text")
+  
+
+
+summary(datos$Ventas.netas)
 
 
 
-salary.hist.density <- ggplot(df, aes(x=Salary, fill = Overpaid)) + 
-  geom_histogram(aes(y = ..density..), binwidth = 3000) + 
-  scale_fill_brewer(palette="Set3") + 
-  geom_vline(xintercept = mid, size = 1, colour = "#FF3721",
-             linetype = "dashed") + 
-  geom_vline(xintercept = med, size = 1, colour = "#2c3e50",
-             linetype = "dashed") + 
-  theme_grey()

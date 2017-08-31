2b1468fac6f367089939c87bb06457eba4804d62
OmarVillalobos <omar_vr@live.com.mx>
2017-08-30 23:36:39 -0500

    added Jan to Jul data

--- Mcklein_Caja.R
+++ Mcklein_Caja.R
@@ -10,26 +10,36 @@ library(lubridate)
 library(tseries)
 library(forecast)
 library(ggplot2)
+library(tidyr)
 
 Sys.setlocale("LC_TIME", "C") 
 
 getwd()
+datos <- read.csv("CAJA-ENE-JUN.csv", header = TRUE)
+datos <- datos %>% unite("Fecha",c("Fecha","Hora"), sep = " ")
+datos$Fecha <- as.POSIXct(strptime(datos$Fecha,"%d/%m/%Y %H:%M:%S"))
+datos$horas <- as.factor(hour(datos$Fecha))
+datos$day <- as.factor(wday(datos$Fecha, label= TRUE, abbr = TRUE))
+datos$month <- as.factor(month(datos$Fecha, label = TRUE, abbr = TRUE))
+datos$Ventas.netas <- as.numeric(datos$Ventas.netas)
+datos <- datos[datos$Fecha < as.Date("2017-06-30 23:00:00"),]
+datos.h1 <- datos %>% select(Fecha, Ventas.netas, day, month, horas)
+
 datos <- read.csv("CAJA-JUL-AGO.csv", header = TRUE)
 datos$Fecha <- as.character(datos$Fecha)
 datos$Fecha <- as.POSIXct(strptime(datos$Fecha,"%d/%m/%y %I:%M %p"))
 datos$horas <- as.factor(hour(datos$Fecha))
+datos$day <- as.factor(wday(datos$Fecha, label= TRUE, abbr = TRUE))
+datos$month <- as.factor(month(datos$Fecha, label = TRUE, abbr = TRUE))
 datos <- datos[datos$Ventas.netas > 0,]
-datos <- datos[datos$Ventas.netas < 500,]
-
+datos.h2 <- datos %>% select(Fecha, Ventas.netas, day, month, horas) 
+datos <- rbind(datos.h1,datos.h2)
 
 mea <- mean(datos$Ventas.netas)
 devup <- mea + sd(datos$Ventas.netas)
 devdow <- mea - sd(datos$Ventas.netas)
-  
-ggplot(datos, aes(x=Ventas.netas,fill=horas)) +
-  geom_histogram(aes(y=..density..), binwidth = 20)
 
-ggplot(datos, aes(horas,Ventas.netas), fill= Total.recaudado) + 
+ggplot(datos, aes(day,Ventas.netas)) + 
   geom_boxplot() + 
   scale_fill_gradient(low = "red", high = "yellow") +
   geom_hline(yintercept = mea, size= 1, colour="#FF3721",
@@ -39,7 +49,15 @@ ggplot(datos, aes(horas,Ventas.netas), fill= Total.recaudado) +
   geom_hline(yintercept = devdow, size= 1, colour="#053FC9",
              linetype = "dashed",show.legend = TRUE) + 
   scale_y_continuous(limits = c(0,500)) + 
-  annotate("text", x = 450, , label = "Some text")
+  annotate("text", x = 5, y= 450, label = "Ventas a la media tarde (5,6,7,8 P.M.)")
+
+  
+ggplot(datos, aes(x=Ventas.netas,fill=horas)) +
+  geom_histogram(aes(y=..density..), binwidth = 20) + 
+  geom_vline(xintercept = mea, size= 1, colour = "#FF3721",
+             linetype= "dashed", show.legend = TRUE)
+
+
   
 
 
